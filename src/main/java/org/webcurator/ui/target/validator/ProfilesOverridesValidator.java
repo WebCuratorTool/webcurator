WebCuratorTool/webcurator-v2-legacy

D1.4.6.3 Text area uses h3RawProfile. h3 sets on ProfileCommand.
The text area for the H3 imported profile override is linked to the
h3RawProfile parameter. The ProfileCommand sets the rawProfile and the
overrideRawProfile to their h3 equivalents.

NOTE:
There is no attempt to scope the overrides based on the base profile.
ALL overrides will be set when the 'apply profile' button is pressed.
For example, if the Base Profile is set to a H1 Profile and an override is
set and then the Base Profile is set to a H3 imported Profile an the XML
is changed and the 'apply profile' button is pressed, all overrides will
be set and a viewing of the target instance will include the overrides
(based on the type of profile selected).

qa-ti-summary.jsp:
- Automatically check the override imported profile checkbox (read only).
- Correct H3 raw profile text area to h3RawProfile which is now used by CodeMirror.

ProfileCommand.java:
- rawProfile set from h3RawProfile
- overrideRawProfile set from overrideH3RawProfile.

TargetInstanceSummaryCommand.java:
- Added h3RawProfile and overrideH3RawProfile

ProfilesOverridesValidator.java:
- Added instrumentation when profile validation succeeds or fails.

wct-core/src/main/java/org/webcurator/ui/target/validator/ProfilesOverridesValidator.java
@@ -15,6 +15,8 @@
  */
 package org.webcurator.ui.target.validator;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 import org.springframework.validation.Errors;
@@ -34,6 +36,7 @@ import org.webcurator.ui.util.HarvestAgentUtil;
  * @author nwaight
  */
 public class ProfilesOverridesValidator extends AbstractBaseValidator implements ApplicationContextAware {
+	private Log log = LogFactory.getLog(ProfilesOverridesValidator.class);
 
 	ApplicationContext applicationContext;
 
@@ -83,8 +86,11 @@ public class ProfilesOverridesValidator extends AbstractBaseValidator implements
 			HarvestAgent harvestAgent = HarvestAgentUtil.getHarvestAgent(getApplicationContext());
 			String rawProfile = command.getRawProfile();
 			if (!harvestAgent.isValidProfile(rawProfile)) {
+				log.info("isOverrideRawProfile rawProfile validation failed.");
 				Object[] vals = new Object[]{"unnamed"};
 				errors.reject("profile.invalid", vals, "The profile is invalid.");
+			} else {
+				log.info("isOverrideRawProfile rawProfile validation succeeded.");
 			}
 		}
 		

WebCuratorTool/webcurator

src/main/java/org/webcurator/ui/target/validator/ProfilesOverridesValidator.java
@@ -15,6 +15,8 @@
  */
 package org.webcurator.ui.target.validator;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 import org.springframework.validation.Errors;
@@ -34,6 +36,7 @@ import org.webcurator.ui.util.HarvestAgentUtil;
  * @author nwaight
  */
 public class ProfilesOverridesValidator extends AbstractBaseValidator implements ApplicationContextAware {
+	private Log log = LogFactory.getLog(ProfilesOverridesValidator.class);
 
 	ApplicationContext applicationContext;
 
@@ -83,8 +86,11 @@ public class ProfilesOverridesValidator extends AbstractBaseValidator implements
 			HarvestAgent harvestAgent = HarvestAgentUtil.getHarvestAgent(getApplicationContext());
 			String h3RawProfile = command.getH3RawProfile();
 			if (!harvestAgent.isValidProfile(h3RawProfile)) {
+				log.info("isOverrideRawProfile h3RawProfile validation failed.");
 				Object[] vals = new Object[]{"unnamed"};
 				errors.reject("profile.invalid", vals, "The profile is invalid.");
+			} else {
+				log.info("isOverrideRawProfile h3RawProfile validation succeeded.");
 			}
 		}
 