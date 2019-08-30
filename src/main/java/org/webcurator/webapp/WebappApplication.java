79984f662ab126b71f0e5b8c600552d06fe6cf14
Hanna Koppelaar <hanna.koppelaar@kb.nl>
2019-08-30 17:23:25 +0200

    Disabled SecurityAutoconfiguration (to get rid of the default login page)

--- src/main/java/org/webcurator/webapp/WebappApplication.java
+++ src/main/java/org/webcurator/webapp/WebappApplication.java
@@ -1,7 +1,9 @@
 package org.webcurator.webapp;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.FilterType;
 import org.webcurator.core.harvester.coordinator.HarvestAgentListenerService;
@@ -12,6 +14,7 @@ import org.webcurator.core.harvester.coordinator.HarvestAgentListenerService;
         // Put any exclusions here.
         //excludeFilters = @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = ClassToExclude.class)
 )
+@EnableAutoConfiguration(exclude = {SecurityAutoConfiguration.class})
 public class WebappApplication {
 
     public static void main(String[] args) {
