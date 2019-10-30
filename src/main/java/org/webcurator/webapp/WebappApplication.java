bc78e158f5a4641ed9dedf3d526f82b3f5752362
Hanna Koppelaar <hanna.koppelaar@kb.nl>
2019-10-30 17:18:47 +0100

    First stab at getting Role management to work

--- src/main/java/org/webcurator/webapp/WebappApplication.java
+++ src/main/java/org/webcurator/webapp/WebappApplication.java
@@ -3,10 +3,9 @@ package org.webcurator.webapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.FilterType;
-import org.webcurator.core.harvester.coordinator.HarvestAgentListenerService;
 
 @SpringBootApplication
 @ComponentScan(basePackages = { "org.webcurator.webapp", "org.webcurator.ui", "org.webcurator.core.harvester",
@@ -14,7 +13,7 @@ import org.webcurator.core.harvester.coordinator.HarvestAgentListenerService;
         // Put any exclusions here.
         //excludeFilters = @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = ClassToExclude.class)
 )
-@EnableAutoConfiguration(exclude = {SecurityAutoConfiguration.class})
+@EnableAutoConfiguration(exclude = {SecurityAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
 public class WebappApplication {
 
     public static void main(String[] args) {
