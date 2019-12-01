ceba9c74e66f7eaf1e4529f098e07d1814e4ba65
leefrank9527 <leefrank9527@gmail.com>
2019-12-02 12:06:06 +1300

    Fixed @RequestMapping and @InitBinder.

--- src/main/java/org/webcurator/ui/target/controller/TargetEditScheduleController.java
+++ src/main/java/org/webcurator/ui/target/controller/TargetEditScheduleController.java
@@ -3,13 +3,19 @@ package org.webcurator.ui.target.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.ServletRequestDataBinder;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+import org.webcurator.ui.target.command.TargetSchedulesCommand;
 import org.webcurator.ui.util.TabbedController;
 
 import javax.annotation.PostConstruct;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @Controller
-@RequestMapping("/curator/target/schedule.html")
 public class TargetEditScheduleController extends EditScheduleController {
     @Autowired
     private ApplicationContext context;
@@ -20,5 +26,16 @@ public class TargetEditScheduleController extends EditScheduleController {
         setScheduleEditPrivilege("ADD_SCHEDULE_TO_TARGET");
         setTargetController((TabbedController) context.getBean("tabbedTargetController"));
         setViewPrefix("target");
+    }
+
+    @InitBinder
+    public void initBinder(HttpServletRequest request, ServletRequestDataBinder binder) throws Exception {
+        super.initBinder(request,binder);
+    }
+
+    @RequestMapping(value = "/curator/target/schedule.html",method = {RequestMethod.GET,RequestMethod.POST})
+    protected ModelAndView handle(@Validated @ModelAttribute("targetSchedulesCommand") TargetSchedulesCommand command,
+                                  HttpServletRequest request, HttpServletResponse response, BindingResult bindingResult)            throws Exception {
+        return  super.handle(command,request,response,bindingResult);
     }
 }
