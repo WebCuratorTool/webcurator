5f9042dab870964d7d76d6cedb2ed575133046e2
leefrank9527 <leefrank9527@gmail.com>
2020-01-10 09:33:38 +1300

    1. Added the component scan path about store archive and exclude HarvestAgentListenerService. 2. Added CommandLineRunner to print scaned components for debug.

--- src/main/java/org/webcurator/core/store/springboot/WebcuratorStoreApplication.java
+++ src/main/java/org/webcurator/core/store/springboot/WebcuratorStoreApplication.java
@@ -1,13 +1,43 @@
 package org.webcurator.core.store.springboot;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.webcurator.core.harvester.coordinator.HarvestAgentListenerService;
+import org.webcurator.core.harvester.coordinator.HarvestCoordinatorImpl;
+
+import java.util.Arrays;
 
 @SpringBootApplication
-@ComponentScan(basePackages = { "org.webcurator.store", "org.webcurator.core.rest" })
+@ComponentScan(basePackages = {"org.webcurator.store", "org.webcurator.core.harvester", "org.webcurator.core.rest", "org.webcurator.core.reader", "org.webcurator.core.store.arc"},
+// HarvestAgentListenerService should be running on webcurator-webapp.
+        excludeFilters = @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE,
+                classes = {HarvestAgentListenerService.class, HarvestCoordinatorImpl.class})
+)
 public class WebcuratorStoreApplication {
     public static void main(String[] args) {
-        SpringApplication.run(WebcuratorStoreApplication.class, args);
+        try {
+            SpringApplication.run(WebcuratorStoreApplication.class, args);
+        } catch (RuntimeException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Bean
+    public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+        return args -> {
+            // Note that this is just here for debugging purposes. It can be deleted at any time.
+            System.out.println("Let's inspect the beans provided by Spring Boot:");
+
+            String[] beanNames = ctx.getBeanDefinitionNames();
+            Arrays.sort(beanNames);
+            for (String beanName : beanNames) {
+                System.out.println(beanName);
+            }
+        };
     }
 }
