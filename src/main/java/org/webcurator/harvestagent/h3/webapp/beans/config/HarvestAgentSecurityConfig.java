2e64a09a47cc8d35e98ee83b90ce4e361e5db93c
leefrank9527 <leefrank9527@gmail.com>
2020-01-09 21:51:06 +1300

    Added configuration items to skip security authentication of restful api access.

--- src/main/java/org/webcurator/harvestagent/h3/webapp/beans/config/HarvestAgentSecurityConfig.java
+++ src/main/java/org/webcurator/harvestagent/h3/webapp/beans/config/HarvestAgentSecurityConfig.java
@@ -1,4 +1,15 @@
 package org.webcurator.harvestagent.h3.webapp.beans.config;
 
-public class HarvestAgentSecutiryConfig {
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class HarvestAgentSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(final HttpSecurity http) throws Exception {
+        http.csrf().disable().authorizeRequests().antMatchers("/**").permitAll();
+    }
 }
