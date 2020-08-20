b354ab5dfcf15f4f531b70d74b0ba9bd22598b37
leefrank9527 <leefrank9527@gmail.com>
2020-08-20 23:21:45 +1200

    Fixed issues of sql script.

--- webcurator-db/set-up-mysql.sh
+++ webcurator-db/set-up-mysql.sh
@@ -4,9 +4,9 @@
 export MYSQL_PWD=password
 
 
-cat latest/setup/wct-create-mysql.sql | mysql -u root
-cat latest/sql/wct-schema-mysql.sql | mysql -u root -D DB_WCT
-cat latest/sql/wct-schema-grants-mysql.sql | mysql -u root -D DB_WCT
-cat latest/sql/wct-indexes-mysql.sql | mysql -u root -D DB_WCT
-cat latest/sql/wct-bootstrap-mysql.sql | mysql -u root -D DB_WCT
-cat latest/sql/wct-qa-data-mysql.sql | mysql -u root -D DB_WCT
+cat latest/src/main/resources/setup/wct-create-mysql.sql | mysql -u root
+cat latest/src/main/resources/sql/wct-schema-mysql.sql | mysql -u root -D DB_WCT
+cat latest/src/main/resources/sql/wct-schema-grants-mysql.sql | mysql -u root -D DB_WCT
+cat latest/src/main/resources/sql/wct-indexes-mysql.sql | mysql -u root -D DB_WCT
+cat latest/src/main/resources/sql/wct-bootstrap-mysql.sql | mysql -u root -D DB_WCT
+cat latest/src/main/resources/sql/wct-qa-data-mysql.sql | mysql -u root -D DB_WCT
--- webcurator-db/set-up-postgresql.sh
+++ webcurator-db/set-up-postgresql.sh
@@ -2,9 +2,9 @@
 
 export PGPASSWORD=password
 
-cat latest/setup/wct-create-postgres.sql | psql -U postgres
-cat latest/sql/wct-schema-postgres.sql | psql -U postgres --dbname=Dwct
-cat latest/sql/wct-schema-grants-postgres.sql | psql -U postgres --dbname=Dwct
-cat latest/sql/wct-indexes-postgres.sql | psql -U postgres --dbname=Dwct
-cat latest/sql/wct-bootstrap-postgres.sql | psql -U postgres --dbname=Dwct
-cat latest/sql/wct-qa-data-postgres.sql | psql -U postgres --dbname=Dwct
+cat latest/src/main/resources/setup/wct-create-postgres.sql | psql -U postgres
+cat latest/src/main/resources/sql/wct-schema-postgres.sql | psql -U postgres --dbname=Dwct
+cat latest/src/main/resources/sql/wct-schema-grants-postgres.sql | psql -U postgres --dbname=Dwct
+cat latest/src/main/resources/sql/wct-indexes-postgres.sql | psql -U postgres --dbname=Dwct
+cat latest/src/main/resources/sql/wct-bootstrap-postgres.sql | psql -U postgres --dbname=Dwct
+cat latest/src/main/resources/sql/wct-qa-data-postgres.sql | psql -U postgres --dbname=Dwct
