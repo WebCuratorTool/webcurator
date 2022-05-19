73a260e977dd85367aef0a1c9fb859dbfb480014
Hanna Koppelaar <33447425+hannakoppelaar@users.noreply.github.com>
2022-05-19 16:55:31 +0200

    Implemented filtering of targets

--- webcurator-webapp/src/main/java/org/webcurator/rest/Targets.java
+++ webcurator-webapp/src/main/java/org/webcurator/rest/Targets.java
@@ -4,25 +4,19 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.webcurator.domain.Pagination;
 import org.webcurator.domain.TargetDAOImpl;
 import org.webcurator.domain.model.core.Seed;
 import org.webcurator.domain.model.core.Target;
 
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.List;
+import java.util.*;
 
 /**
  * Handlers for the targets endpoint
  */
 @RestController
-@RequestMapping(path = "/api/{version}/targets/")
+@RequestMapping(path = "/api/{version}/targets")
 public class Targets {
 
 
@@ -31,35 +25,78 @@ public class Targets {
     @Autowired
     private TargetDAOImpl targetDAO;
 
-    @GetMapping
-    public ResponseEntity<?> find(@PathVariable String version) {
-        Pagination pagination = targetDAO.search(0, 10, null, null,
-                                                null, null, null, null, null,
-                                                false, null, null);
-        List<TargetSummary> targetSummaries = new ArrayList<>();
-        for (Target t : (List<Target>)pagination.getList()) {
-            targetSummaries.add(new TargetSummary(t));
-        }
+    @GetMapping(path = "")
+    public ResponseEntity<?> get(@RequestParam(required = false) Long targetId, @RequestParam(required = false) String name,
+                                 @RequestParam(required = false) String seed, @RequestParam(required = false) String agency,
+                                 @RequestParam(required = false) String userId, @RequestParam(required = false) String description,
+                                 @RequestParam(required = false) String groupName, @RequestParam(required = false) boolean nonDisplayOnly,
+                                 @RequestParam(required = false) Set<Integer> states) {
+        Filter filter = new Filter(targetId, name, seed, agency, userId, description, groupName, nonDisplayOnly, states);
+        List<TargetSummary> targetSummaries = search(filter);
         ResponseEntity<List<TargetSummary>> response = ResponseEntity.ok(targetSummaries);
         return response;
     }
 
+    @GetMapping(path = "/{targetId}")
+    public ResponseEntity<?> get(@RequestParam int targetId) {
+        return null;
+    }
+
+    private List<TargetSummary> search(Filter filter) {
+        // TODO implement paging and sorting
+        Pagination pagination = targetDAO.search(0, 10, filter.targetId, filter.name,
+                filter.states, filter.seed, filter.userId, filter.agency, filter.groupName,
+                filter.nonDisplayOnly, null, filter.description);
+        List<TargetSummary> targetSummaries = new ArrayList<>();
+        for (Target t : (List<Target>)pagination.getList()) {
+            targetSummaries.add(new TargetSummary(t));
+        }
+        return targetSummaries;
+    }
+
+    /**
+     * Wrapper for the search filter
+     */
+    private class Filter {
+        Long targetId;
+        String name;
+        String seed;
+        String agency;
+        String userId;
+        String description; // Spec; General.Description
+        String groupName; // Spec: Group.Name
+        boolean nonDisplayOnly; // Spec: Access.AccessZone as a human-readable string
+        Set<Integer> states; // Spec: General.State as human-readable string
+
+        Filter(Long targetId, String name, String seed, String agency, String userId, String description,
+                String groupName, boolean nonDisplayOnly, Set<Integer> states) {
+            this.targetId = targetId;
+            this.name = name;
+            this.seed = seed;
+            this.agency = agency;
+            this.userId = userId;
+            this.description = description;
+            this.groupName = groupName;
+            this.nonDisplayOnly = nonDisplayOnly;
+            this.states = states;
+        }
+    }
 
     /**
      * Wraps summary info about a target, gets directly mapped to JSON
      */
     private class TargetSummary {
 
-        private long targetId;
-        private Date creationDate;
-        private String name;
-        private String agency;
-        private String owner;
+        public long targetId;
+        public Date creationDate;
+        public String name;
+        public String agency;
+        public String owner;
         // FIXME we don't want the front-end to map integers to human-readable states (as happens in the JSPs)
-        private int status;
-        private List<HashMap<String, String>> seeds;
+        public int status;
+        public List<HashMap<String, String>> seeds;
 
-        public TargetSummary(Target t) {
+        TargetSummary(Target t) {
             this.targetId = t.getOid();
             this.creationDate = t.getCreationDate();
             this.name = t.getName();
@@ -73,63 +110,6 @@ public class Targets {
                 seed.put("primary", Boolean.toString(s.isPrimary()));
                 seeds.add(seed);
             }
-        }
-
-
-        public long getTargetId() {
-            return targetId;
-        }
-
-        public void setTargetId(long targetId) {
-            this.targetId = targetId;
-        }
-
-        public Date getCreationDate() {
-            return creationDate;
-        }
-
-        public void setCreationDate(Date creationDate) {
-            this.creationDate = creationDate;
-        }
-
-        public String getName() {
-            return name;
-        }
-
-        public void setName(String name) {
-            this.name = name;
-        }
-
-        public String getAgency() {
-            return agency;
-        }
-
-        public void setAgency(String agency) {
-            this.agency = agency;
-        }
-
-        public String getOwner() {
-            return owner;
-        }
-
-        public void setOwner(String owner) {
-            this.owner = owner;
-        }
-
-        public int getStatus() {
-            return status;
-        }
-
-        public void setStatus(int status) {
-            this.status = status;
-        }
-
-        public List<HashMap<String, String>> getSeeds() {
-            return seeds;
-        }
-
-        public void setSeeds(List<HashMap<String, String>> seeds) {
-            this.seeds = seeds;
         }
     }
 }
