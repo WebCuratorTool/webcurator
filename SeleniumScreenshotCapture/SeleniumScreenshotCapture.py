cbb2512f6bbc1df9cd70d4c7101f7cbb55e60c6e
leefrank9527 <leefrank9527@gmail.com>
2022-05-31 11:40:03 +1200

    SeleniumScreenshotCapture: changed the variable names and methods to fulfill the lints

--- SeleniumScreenshotCapture/SeleniumScreenshotCapture.py
+++ SeleniumScreenshotCapture/SeleniumScreenshotCapture.py
@@ -5,81 +5,90 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions
 from PIL import Image
 
+
 def createThumbnail(input, output):
     sourceImage = Image.open(input)
     resized = sourceImage.resize((200, 150))
     resized.save(output)
     sourceImage.close()
 
+
 # Main
-commandArgs = sys.argv
-url = None
-filepath = None
-width = None
-height = None
-server = None
-isWayback = False
+def main(command_args):
+    url = None
+    filepath = None
+    width = None
+    height = None
+    server = None
+    is_wayback = False
 
-# Assign argument values to variables
-for arg in commandArgs:
-    if arg == "--wayback":
-        isWayback = True
-        continue
-    if "SeleniumScreenshotCapture.py" in arg:
-        continue
-    keyVals = arg.split("=")
-    if keyVals[0] == "url":
-        url = keyVals[1]
-    elif keyVals[0] == "filepath":
-        filepath = keyVals[1]
-    elif keyVals[0] == "width":
-        width = keyVals[1]
-    elif keyVals[0] == "height":
-        height = keyVals[1]
-    elif keyVals[0] == "selenium-server":
-        server = keyVals[1]
+    # Assign argument values to variables
+    for arg in command_args:
+        if arg == "--wayback":
+            is_wayback = True
+            continue
+        if "SeleniumScreenshotCapture.py" in arg:
+            continue
+        key_vals = arg.split("=")
+        if key_vals[0] == "url":
+            url = key_vals[1]
+        elif key_vals[0] == "filepath":
+            filepath = key_vals[1]
+        elif key_vals[0] == "width":
+            width = key_vals[1]
+        elif key_vals[0] == "height":
+            height = key_vals[1]
+        elif key_vals[0] == "selenium-server":
+            server = key_vals[1]
+        else:
+            print("Unrecognised argument, cannot generate screenshots.  Arg: " + arg)
+            sys.exit(1)
+
+    # file_directory = Path(filepath).parent
+    # file_directory.mkdir(parents=True, exist_ok=True)
+
+    # Set up the web driver
+    chromeOptions = webdriver.ChromeOptions()
+    chromeOptions.headless = True
+    if server is not None:
+        driver = webdriver.Remote(command_executor=server, options=chromeOptions)
     else:
-        print("Unrecognised argument, cannot generate screenshots.  Arg: " + arg)
-        sys.exit(1)
+        driver = webdriver.Chrome(options=chromeOptions)
 
-# Set up the web driver
-chromeOptions = webdriver.ChromeOptions()
-chromeOptions.headless = True
-if server is not None:
-    driver = webdriver.Remote(command_executor=server, options=chromeOptions)
-else:
-    driver = webdriver.Chrome(options=chromeOptions)
+    driver.get(url)
 
-driver.get(url)
+    if is_wayback:
+        # Remove wayback banner and modify the iframe
+        element = driver.find_element("wb_div")
+        driver.execute_script("return document.getElementsByTagName('wb_div')[0].remove();")
+        driver.execute_script("return document.getElementById('wb_iframe_div').setAttribute('style','padding:0px 0px 0px 0px');")
 
-if isWayback:
-    # Remove wayback banner and modify the iframe
-    element = driver.find_element_by_tag_name("wb_div")
-    driver.execute_script("return document.getElementsByTagName('wb_div')[0].remove();")
-    driver.execute_script("return document.getElementById('wb_iframe_div').setAttribute('style','padding:0px 0px 0px 0px');")
+        # Wait for frame to be ready then switch the focus to the frame
+        wait = WebDriverWait(driver, 10)
+        replay_frame = wait.until(expected_conditions.visibility_of_element_located((By.ID, "replay_iframe")))
+        driver.switch_to.frame("replay_iframe")
 
-    # Wait for frame to be ready then switch the focus to the frame
-    wait = WebDriverWait(driver, 10)
-    replay_frame = wait.until(expected_conditions.visibility_of_element_located((By.ID, "replay_iframe")))
-    driver.switch_to.frame("replay_iframe")
+    # Set screenshot size
+    if width is None and height is None:
+        S = lambda X: driver.execute_script('return document.body.parentNode.scroll' + X)
+        driver.set_window_size(S('Width'), S('Height'))
+    else:
+        driver.set_window_size(width, height)
 
-# Set screenshot size 
-if width is None and height is None: 
-    S = lambda X: driver.execute_script('return document.body.parentNode.scroll'+X)
-    driver.set_window_size(S('Width'),S('Height'))
-else:
-    driver.set_window_size(width, height)
+    driver.save_screenshot(filepath)
+    driver.close()
 
-driver.save_screenshot(filepath)
-driver.close()
+    # Generate screenshot thumbnails
+    size = None
+    if "screen" in filepath:
+        size = "screen"
+    elif "fullpage" in filepath:
+        size = "fullpage"
 
-# Generate screenshot thumbnails
-size = None
-if "screen" in filepath:
-    size = "screen"
-elif "fullpage" in filepath:
-    size = "fullpage"
+    createThumbnail(filepath, filepath.replace(size, size + "-thumbnail"))
 
-createThumbnail(filepath, filepath.replace(size, size + "-thumbnail"))
+    sys.exit(0)
 
-sys.exit(0)
+
+if __name__ == "__main__":
+    main(sys.argv)
