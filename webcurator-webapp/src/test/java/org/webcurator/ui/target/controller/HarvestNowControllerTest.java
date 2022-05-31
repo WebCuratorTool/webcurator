0e168bcd419ec86689284c3e8e0fa5a7047ae71c
leefrank9527 <leefrank9527@gmail.com>
2022-05-31 23:57:21 +1200

    Fixed the test cases

--- webcurator-webapp/src/test/java/org/webcurator/ui/target/controller/HarvestNowControllerTest.java
+++ webcurator-webapp/src/test/java/org/webcurator/ui/target/controller/HarvestNowControllerTest.java
@@ -1,10 +1,12 @@
 package org.webcurator.ui.target.controller;
 
-import static org.junit.Assert.assertNull;
-import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import static org.mockito.Matchers.any;
 
 import java.util.HashMap;
+import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -21,6 +23,9 @@ import org.webcurator.core.harvester.coordinator.HarvestAgentManagerImpl;
 import org.webcurator.core.coordinator.WctCoordinatorImpl;
 import org.webcurator.core.notification.MockInTrayManager;
 import org.webcurator.core.scheduler.MockTargetInstanceManager;
+import org.webcurator.core.store.DigitalAssetStore;
+import org.webcurator.core.store.DigitalAssetStoreFactory;
+import org.webcurator.core.store.DigitalAssetStoreFactoryImpl;
 import org.webcurator.core.targets.MockTargetManager;
 import org.webcurator.domain.MockTargetInstanceDAO;
 import org.webcurator.domain.TargetInstanceDAO;
@@ -34,161 +39,160 @@ import org.webcurator.ui.target.validator.MockHarvestNowValidator;
 
 public class HarvestNowControllerTest extends BaseWCTTest<HarvestNowController> {
 
-	private TargetInstanceDAO tidao;
-	private WctCoordinatorImpl hc;
-	public HarvestNowControllerTest()
-	{
-		super(HarvestNowController.class,
-				"/org/webcurator/ui/target/controller/HarvestNowControllerTest.xml");
+    private TargetInstanceDAO tidao;
+    private WctCoordinatorImpl hc;
 
-	}
+    public HarvestNowControllerTest() {
+        super(HarvestNowController.class,
+                "/org/webcurator/ui/target/controller/HarvestNowControllerTest.xml");
 
-	//Override BaseWCTTest setup method
-	public void setUp() throws Exception {
-		//call the overridden method as well
-		super.setUp();
+    }
 
-		//add the extra bits
-		tidao = new MockTargetInstanceDAO(testFile);
+    //Override BaseWCTTest setup method
+    public void setUp() throws Exception {
+        //call the overridden method as well
+        super.setUp();
 
-		DateUtils.get().setMessageSource(new MockMessageSource());
-		MockTargetInstanceManager tim = new MockTargetInstanceManager(testFile);
+        //add the extra bits
+        tidao = new MockTargetInstanceDAO(testFile);
 
-		tim.setTargetInstanceDao(tidao);
+        DateUtils.get().setMessageSource(new MockMessageSource());
+        MockTargetInstanceManager tim = new MockTargetInstanceManager(testFile);
 
-		hc = new WctCoordinatorImpl();
+        tim.setTargetInstanceDao(tidao);
 
-		hc.setTargetInstanceManager(tim);
-		hc.setTargetManager(new MockTargetManager(testFile));
-		hc.setInTrayManager(new MockInTrayManager(testFile));
-		hc.setSipBuilder(new MockSipBuilder(testFile));
-		HarvestAgentManagerImpl harvestAgentManager = new HarvestAgentManagerImpl();
-		harvestAgentManager.setHarvestAgentFactory(new MockHarvestAgentFactory());
-		harvestAgentManager.setTargetInstanceManager(tim);
-		harvestAgentManager.setTargetInstanceDao(tidao);
-		hc.setHarvestAgentManager(harvestAgentManager);
-		hc.setTargetInstanceDao(tidao);
+        hc = new WctCoordinatorImpl();
 
-		testInstance.setWctCoordinator(hc);
-		testInstance.setTargetInstanceDAO(tidao);
-		testInstance.setMessageSource(new MockMessageSource());
-	}
+        hc.setTargetInstanceManager(tim);
+        hc.setTargetManager(new MockTargetManager(testFile));
+        hc.setInTrayManager(new MockInTrayManager(testFile));
+        hc.setSipBuilder(new MockSipBuilder(testFile));
+        HarvestAgentManagerImpl harvestAgentManager = new HarvestAgentManagerImpl();
+        harvestAgentManager.setHarvestAgentFactory(new MockHarvestAgentFactory());
+        harvestAgentManager.setTargetInstanceManager(tim);
+        harvestAgentManager.setTargetInstanceDao(tidao);
+        hc.setHarvestAgentManager(harvestAgentManager);
+        hc.setTargetInstanceDao(tidao);
 
-	@Test
-	public final void testShowForm() {
-		try
-		{
-			assertNull(testInstance.showForm());
-		}
-		catch(Exception e)
-		{
-			fail(e.getMessage());
-		}
-	}
+        testInstance.setWctCoordinator(hc);
+        testInstance.setTargetInstanceDAO(tidao);
+        testInstance.setMessageSource(new MockMessageSource());
+    }
 
-	@Test
-	public final void testProcessFormSubmission() throws Exception {
+    @Test
+    public final void testShowForm() {
+        try {
+            assertNull(testInstance.showForm());
+        } catch (Exception e) {
+            fail(e.getMessage());
+        }
+    }
 
-		TargetInstance ti = tidao.load(5001L);
-		ti.setState(TargetInstance.STATE_SCHEDULED);
+    @Test
+    public final void testProcessFormSubmission() throws Exception {
 
-		HashMap<String, HarvesterStatusDTO> aHarvesterStatus = new HashMap<String, HarvesterStatusDTO>();
+        TargetInstance ti = tidao.load(5001L);
+        ti.setState(TargetInstance.STATE_SCHEDULED);
 
-		aHarvesterStatus.put("Target-5002", getStatusDTO("Running"));
-		HarvestAgentStatusDTO aStatus = new HarvestAgentStatusDTO();
-		aStatus.setName("Test Agent");
-		aStatus.setHarvesterStatus(aHarvesterStatus);
-		aStatus.setMaxHarvests(2);
-		hc.heartbeat(aStatus);
+        HashMap<String, HarvesterStatusDTO> aHarvesterStatus = new HashMap<String, HarvesterStatusDTO>();
+
+        aHarvesterStatus.put("Target-5002", getStatusDTO("Running"));
+        HarvestAgentStatusDTO aStatus = new HarvestAgentStatusDTO();
+        aStatus.setName("Test Agent");
+        aStatus.setHarvesterStatus(aHarvesterStatus);
+        aStatus.setMaxHarvests(2);
+        hc.heartbeat(aStatus);
 
 
-		HttpServletRequest aReq = new MockHttpServletRequest();
-		TargetInstanceCommand aCmd = new TargetInstanceCommand();
-		aCmd.setCmd(TargetInstanceCommand.ACTION_HARVEST);
-		aCmd.setAgent("Test Agent");
-		aCmd.setTargetInstanceId(5001L);
-		BindingResult bindingResult = new BindException(aCmd, TargetInstanceCommand.ACTION_HARVEST);
-		ReflectionTestUtils.setField(testInstance, "harvestNowValidator", new MockHarvestNowValidator());
+        HttpServletRequest aReq = new MockHttpServletRequest();
+        TargetInstanceCommand aCmd = new TargetInstanceCommand();
+        aCmd.setCmd(TargetInstanceCommand.ACTION_HARVEST);
+        aCmd.setAgent("Test Agent");
+        aCmd.setTargetInstanceId(5001L);
+        BindingResult bindingResult = new BindException(aCmd, TargetInstanceCommand.ACTION_HARVEST);
+        ReflectionTestUtils.setField(testInstance, "harvestNowValidator", new MockHarvestNowValidator());
 
-		ModelAndView mav = testInstance.processFormSubmission(aCmd, bindingResult, aReq);
-		assertTrue(mav != null);
-		assertTrue(ti.getState().equals(TargetInstance.STATE_RUNNING));
-	}
+        DigitalAssetStoreFactory mockDasFactory = mock(DigitalAssetStoreFactory.class);
+        hc.setDigitalAssetStoreFactory(mockDasFactory);
+        DigitalAssetStore mockDas = mock(DigitalAssetStore.class);
+        when(mockDasFactory.getDAS()).thenReturn(mockDas);
+        when(mockDasFactory.getDAS().createScreenshots(any(Map.class))).thenReturn(true);
 
-	@Test
-	public final void testProcessFormSubmissionPaused() {
+        ModelAndView mav = testInstance.processFormSubmission(aCmd, bindingResult, aReq);
+        assertNotNull(mav);
+        assertEquals(TargetInstance.STATE_RUNNING, ti.getState());
+    }
 
-		TargetInstance ti = tidao.load(5001L);
-		ti.setState(TargetInstance.STATE_SCHEDULED);
+    @Test
+    public final void testProcessFormSubmissionPaused() {
 
-		hc.pauseQueue();
+        TargetInstance ti = tidao.load(5001L);
+        ti.setState(TargetInstance.STATE_SCHEDULED);
 
-		HashMap<String, HarvesterStatusDTO> aHarvesterStatus = new HashMap<String, HarvesterStatusDTO>();
+        hc.pauseQueue();
 
-		aHarvesterStatus.put("Target-5002", getStatusDTO("Running"));
-		HarvestAgentStatusDTO aStatus = new HarvestAgentStatusDTO();
-		aStatus.setName("Test Agent");
-		aStatus.setHarvesterStatus(aHarvesterStatus);
-		aStatus.setMaxHarvests(2);
-		hc.heartbeat(aStatus);
+        HashMap<String, HarvesterStatusDTO> aHarvesterStatus = new HashMap<String, HarvesterStatusDTO>();
 
-
-		HttpServletRequest aReq = new MockHttpServletRequest();
-		TargetInstanceCommand aCmd = new TargetInstanceCommand();
-		aCmd.setCmd(TargetInstanceCommand.ACTION_HARVEST);
-		aCmd.setAgent("Test Agent");
-		aCmd.setTargetInstanceId(5001L);
-		BindingResult bindingResult = new BindException(aCmd, TargetInstanceCommand.ACTION_HARVEST);
-		ReflectionTestUtils.setField(testInstance, "harvestNowValidator", new MockHarvestNowValidator());
-
-		try
-		{
-			ModelAndView mav = testInstance.processFormSubmission(aCmd, bindingResult, aReq);
-			assertTrue(mav != null);
-			assertTrue(ti.getState().equals(TargetInstance.STATE_SCHEDULED));
-		}
-		catch(Exception e)
-		{
-			fail(e.getMessage());
-		}
-	}
-
-	@Test
-	public final void testProcessFormSubmissionAgentPaused() throws Exception {
-
-		TargetInstance ti = tidao.load(5001L);
-		ti.setState(TargetInstance.STATE_SCHEDULED);
-
-		HashMap<String, HarvesterStatusDTO> aHarvesterStatus = new HashMap<String, HarvesterStatusDTO>();
-
-		aHarvesterStatus.put("Target-5002", getStatusDTO("Running"));
-		HarvestAgentStatusDTO aStatus = new HarvestAgentStatusDTO();
-		aStatus.setName("Test Agent");
-		aStatus.setHarvesterStatus(aHarvesterStatus);
-		aStatus.setMaxHarvests(2);
-
-		aStatus.setAcceptTasks(false);
-
-		hc.heartbeat(aStatus);
+        aHarvesterStatus.put("Target-5002", getStatusDTO("Running"));
+        HarvestAgentStatusDTO aStatus = new HarvestAgentStatusDTO();
+        aStatus.setName("Test Agent");
+        aStatus.setHarvesterStatus(aHarvesterStatus);
+        aStatus.setMaxHarvests(2);
+        hc.heartbeat(aStatus);
 
 
-		HttpServletRequest aReq = new MockHttpServletRequest();
-		TargetInstanceCommand aCmd = new TargetInstanceCommand();
-		aCmd.setCmd(TargetInstanceCommand.ACTION_HARVEST);
-		aCmd.setAgent("Test Agent");
-		aCmd.setTargetInstanceId(5001L);
-		BindingResult bindingResult = new BindException(aCmd, TargetInstanceCommand.ACTION_HARVEST);
-		ReflectionTestUtils.setField(testInstance, "harvestNowValidator", new MockHarvestNowValidator());
+        HttpServletRequest aReq = new MockHttpServletRequest();
+        TargetInstanceCommand aCmd = new TargetInstanceCommand();
+        aCmd.setCmd(TargetInstanceCommand.ACTION_HARVEST);
+        aCmd.setAgent("Test Agent");
+        aCmd.setTargetInstanceId(5001L);
+        BindingResult bindingResult = new BindException(aCmd, TargetInstanceCommand.ACTION_HARVEST);
+        ReflectionTestUtils.setField(testInstance, "harvestNowValidator", new MockHarvestNowValidator());
 
-		ModelAndView mav = testInstance.processFormSubmission(aCmd, bindingResult, aReq);
-		assertTrue(mav != null);
-		assertTrue(ti.getState().equals(TargetInstance.STATE_SCHEDULED));
-	}
+        try {
+            ModelAndView mav = testInstance.processFormSubmission(aCmd, bindingResult, aReq);
+            assertTrue(mav != null);
+            assertTrue(ti.getState().equals(TargetInstance.STATE_SCHEDULED));
+        } catch (Exception e) {
+            fail(e.getMessage());
+        }
+    }
 
-	private HarvesterStatusDTO getStatusDTO(String aStatus)
-	{
-		HarvesterStatusDTO sdto = new HarvesterStatusDTO();
-		sdto.setStatus(aStatus);
-		return sdto;
-	}
+    @Test
+    public final void testProcessFormSubmissionAgentPaused() throws Exception {
+
+        TargetInstance ti = tidao.load(5001L);
+        ti.setState(TargetInstance.STATE_SCHEDULED);
+
+        HashMap<String, HarvesterStatusDTO> aHarvesterStatus = new HashMap<String, HarvesterStatusDTO>();
+
+        aHarvesterStatus.put("Target-5002", getStatusDTO("Running"));
+        HarvestAgentStatusDTO aStatus = new HarvestAgentStatusDTO();
+        aStatus.setName("Test Agent");
+        aStatus.setHarvesterStatus(aHarvesterStatus);
+        aStatus.setMaxHarvests(2);
+
+        aStatus.setAcceptTasks(false);
+
+        hc.heartbeat(aStatus);
+
+
+        HttpServletRequest aReq = new MockHttpServletRequest();
+        TargetInstanceCommand aCmd = new TargetInstanceCommand();
+        aCmd.setCmd(TargetInstanceCommand.ACTION_HARVEST);
+        aCmd.setAgent("Test Agent");
+        aCmd.setTargetInstanceId(5001L);
+        BindingResult bindingResult = new BindException(aCmd, TargetInstanceCommand.ACTION_HARVEST);
+        ReflectionTestUtils.setField(testInstance, "harvestNowValidator", new MockHarvestNowValidator());
+
+        ModelAndView mav = testInstance.processFormSubmission(aCmd, bindingResult, aReq);
+        assertTrue(mav != null);
+        assertTrue(ti.getState().equals(TargetInstance.STATE_SCHEDULED));
+    }
+
+    private HarvesterStatusDTO getStatusDTO(String aStatus) {
+        HarvesterStatusDTO sdto = new HarvesterStatusDTO();
+        sdto.setStatus(aStatus);
+        return sdto;
+    }
 }
