fb31816644a321f8e32f1268524a87eaf7aa2132
Hanna Koppelaar <33447425+hannakoppelaar@users.noreply.github.com>
2022-10-19 16:30:05 +0200

    Use jwarc to generate cdx - fix for #70

--- webcurator-core/src/main/java/org/archive/io/CDXIndexer.java
+++ webcurator-core/src/main/java/org/archive/io/CDXIndexer.java
@@ -1,14 +1,22 @@
 package org.archive.io;
 
-import java.io.File;
-import java.io.IOException;
-
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.netpreserve.jwarc.*;
+import org.netpreserve.jwarc.cdx.CdxFormat;
+import org.netpreserve.jwarc.cdx.CdxRequestEncoder;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.webcurator.core.store.IndexerBase;
 import org.webcurator.core.store.RunnableIndex;
 import org.webcurator.domain.model.core.HarvestResultDTO;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.net.URI;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class CDXIndexer extends IndexerBase {
     private static Log log = LogFactory.getLog(CDXIndexer.class);
@@ -30,13 +38,57 @@ public class CDXIndexer extends IndexerBase {
         enabled = original.enabled;
     }
 
+    /**
+     * Adapted from the jwarc CdxTool
+     *
+     * @param archiveFile
+     * @throws IOException
+     */
     private void writeCDXIndex(File archiveFile) throws IOException {
-        ArchiveReader r;
-        r = ArchiveReaderFactory.get(archiveFile);
-        r.setStrict(false);
-        r.setDigest(true);
-        r.cdxOutput(true);
-        r.close();
+        CdxFormat cdxFormat = CdxFormat.CDX11;
+        String cdxFilename = getCdxFilename(archiveFile);
+        try (WarcReader reader = new WarcReader(archiveFile.toPath());
+             BufferedWriter cdxWriter = new BufferedWriter(new FileWriter(cdxFilename))) {
+            reader.onWarning(log::warn);
+            WarcRecord record = reader.next().orElse(null);
+            String filename = archiveFile.getName();
+
+            // Write cdx header
+            cdxWriter.write(" CDX " + cdxFormat.legend());
+            cdxWriter.newLine();
+            while (record != null) {
+                try {
+                    if ((record instanceof WarcResponse || record instanceof WarcResource) &&
+                            ((WarcCaptureRecord) record).payload().isPresent()) {
+                        long position = reader.position();
+                        WarcCaptureRecord capture = (WarcCaptureRecord) record;
+                        URI id = record.version().getProtocol().equals("ARC") ? null : record.id();
+
+                        // advance to the next record so we can calculate the length
+                        record = reader.next().orElse(null);
+                        long length = reader.position() - position;
+
+                        cdxWriter.write(cdxFormat.format(capture, filename, position, length));
+                        cdxWriter.newLine();
+                    } else {
+                        record = reader.next().orElse(null);
+                    }
+                } catch (ParsingException e) {
+                    log.error("ParsingException at record " + reader.position() + ": " + e.getMessage());
+                    record = reader.next().orElse(null);
+                }
+            }
+        }
+    }
+
+    private String getCdxFilename(File archiveFile) {
+        String cdxFilename = archiveFile.getAbsolutePath();
+        String suffixRegex = ".w?arc(.gz)?$";
+        Pattern p = Pattern.compile(suffixRegex, Pattern.CASE_INSENSITIVE);
+        Matcher m = p.matcher(cdxFilename);
+        cdxFilename = m.replaceAll("");
+        cdxFilename += ".cdx";
+        return cdxFilename;
     }
 
     @Override
