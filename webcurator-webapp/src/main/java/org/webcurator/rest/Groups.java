1c1e8836b829087ca27965de3f77e3cde30d948a
Hanna Koppelaar <33447425+hannakoppelaar@users.noreply.github.com>
2023-09-08 18:16:24 +0200

    Change GET-with-request-body to PATCH (JavaScript doesn't support the former)

--- webcurator-webapp/src/main/java/org/webcurator/rest/Groups.java
+++ webcurator-webapp/src/main/java/org/webcurator/rest/Groups.java
@@ -1,11 +1,9 @@
 package org.webcurator.rest;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.webcurator.domain.Pagination;
 import org.webcurator.domain.TargetDAO;
 import org.webcurator.domain.model.core.TargetGroup;
@@ -30,7 +28,7 @@ public class Groups {
     @Autowired
     TargetDAO targetDAO;
 
-    @GetMapping(path = "")
+    @PatchMapping(path = "", consumes = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity get(@RequestBody(required = false) SearchParams searchParams) {
        if (searchParams == null) {
             searchParams = new SearchParams();
