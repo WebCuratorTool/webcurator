a8037166b5a59ff47cb26067dce733dde3fb5f57
jeremy-liss <jeremyliss@gmail.com>
2024-03-19 16:29:02 +1300

    initial commit for ui

---
+++ webcurator-ui/src/components/NavBar.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="nav-bar">
+        <img class="logo" src="../assets/wct_logo.png" />
+    </div>
+</template>
+
+<style scoped>
+.nav-bar{
+    position: sticky;
+    height: 80px;
+    width: 100%;
+    background-color: #1F5384;
+}
+
+.logo {
+    height: 80%;
+    margin: 0;
+    padding-left: 15px;
+    position: absolute;
+    top: 50%;
+    -ms-transform: translateY(-50%);
+    transform: translateY(-50%);
+}
+</style>
