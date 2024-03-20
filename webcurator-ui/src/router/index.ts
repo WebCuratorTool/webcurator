456d26aac2c8dca3961e40403d3872a5edf67917
jeremy-liss <jeremyliss@gmail.com>
2024-03-20 14:47:34 +1300

    Basic Vue setup for WCT

--- webcurator-ui/src/router/index.ts
+++ webcurator-ui/src/router/index.ts
@@ -1,21 +1,21 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import Dashboard from '../views/Dashboard.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView
+      name: 'dashboard',
+      component: Dashboard
     },
     {
-      path: '/about',
-      name: 'about',
+      path: '/targets',
+      name: 'targets',
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
+      component: () => import('../views/Targets.vue')
     }
   ]
 })
