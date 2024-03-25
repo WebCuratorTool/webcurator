82db387193f5442a0e416c8d2988a731cd442d5c
“leefrank9527” <leefrank9527@gmail.com>
2024-03-25 22:35:03 +1300

    ui: added the dynamic login dialog

--- webcurator-ui/src/components/LoginDialog.vue
+++ webcurator-ui/src/components/LoginDialog.vue
@@ -1,50 +1,79 @@
 
 <template>
 	<div>
+        <!-- <div class="text-center mb-5" style="padding: 0px 100px 5px 100px;">
+            <img src="@/assets/wct_logo.png" alt="Image" height="50" class="mb-3" />            
+        </div> -->
+
         <div class="flex align-items-center gap-3 mb-3">
             <label for="username" class="font-semibold w-6rem">Username</label>
-            <InputText id="username" class="flex-auto" autocomplete="off" v-model="username" />
+            <br/>
+            <InputText id="username" type="text" placeholder="Username" class="w-full mb-3" v-model="username"/>
         </div>
-        <div class="flex align-items-center gap-3 mb-5">
+        <br/>
+        <div class="flex align-items-center gap-3 mb-3">
             <label for="password" class="font-semibold w-6rem">Password</label>
-            <Password id="password" class="flex-auto" autocomplete="off"  v-model="password" toggleMask/>
+            <br/>
+            <InputText id="password" type="password" placeholder="Password" class="w-full mb-3" v-model="password"/>
         </div>
+
+        <br/>
+        <Divider />
+        <br/>
+
         <div class="flex justify-content-end gap-2">
-            <Button type="button" label="Cancel" severity="secondary" @click="visible = false"></Button>
-            <Button type="button" label="Save" @click="visible = false"></Button>
+            <Button type="button" icon="pi pi-user" label="login"  class="w-full p-3 text-xl" @click="auth"></Button>
         </div>
 	</div>
 </template>
 
 <script setup lang="ts">
 import { ref, onMounted, inject } from "vue";
+// import { useToast } from 'primevue/usetoast';
+
+// const toast = useToast();
 
 const dialogRef:any = inject("dialogRef");
-const username=ref("leefr");
-const password=ref("password")
+const username=ref(null);
+const password=ref(null);
 
-onMounted(() => {
-    
-});
 
-const auth = async () => {
-    var url="./auth/v1/token?username=lql&password=1qaz@WSX";
-    const response = await fetch(url, {
+const auth = () => {
+    var url="./auth/v1/token?username=" + username.value + "&password=" + password.value;
+    fetch(url, {
         method: 'POST',
         redirect: 'follow',
         headers: {
             'Content-Type': 'application/json',
         }
+    }).then((rsp)=>{
+        console.log(rsp);
+        if(!rsp.ok){
+            throw new Error(rsp.status + " : " + rsp.statusText);
+        }
+        return rsp.text();
+    })
+    .then((token)=>{
+        console.log("token:" + token);
+        dialogRef.value.close(token);
+    }).catch((err)=>{
+        console.log(err);
+        // toast.add({ severity: 'error', summary: 'Error Message', detail: err.message, life: 3000 });
+        alert(err.message);
     });
-    return response.text();
 };
 
 
-const selectProduct = (data:any) => {
-    dialogRef.value.close(data);
-};
 
-const showInfo = () => {
-    
-};
+
 </script>
+
+<style scoped>
+    .p-inputtext{
+        width: 300px;
+    }
+    
+    .p-button{
+        width: 300px;
+    }
+</style>
