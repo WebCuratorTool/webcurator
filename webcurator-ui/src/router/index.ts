a55c41c0c4856dc07368f1d5abd12bd18193b32e
“leefrank9527” <leefrank9527@gmail.com>
2024-04-02 16:26:19 +1300

    ui: set the context path of routes

--- webcurator-ui/src/router/index.ts
+++ webcurator-ui/src/router/index.ts
@@ -5,18 +5,23 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'dashboard',
-      component: Dashboard
-    },
-    {
-      path: '/targets',
-      name: 'targets',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/Targets.vue')
-    }
+      path: '/wct',
+      children: [
+        {
+          path: 'dashboard',
+          name: 'dashboard',
+          component: Dashboard
+        },
+        {
+          path: 'targets',
+          name: 'targets',
+          // route level code-splitting
+          // this generates a separate chunk (About.[hash].js) for this route
+          // which is lazy-loaded when the route is visited.
+          component: () => import('../views/Targets.vue')
+        }
+      ]
+    }   
   ]
 })
 
