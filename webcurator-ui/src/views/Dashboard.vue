62f93a701d67213875b8d763822d1902f510695f
“leefrank9527” <leefrank9527@gmail.com>
2024-04-02 16:35:58 +1300

    ui: added the example for how to use the http client

--- webcurator-ui/src/views/Dashboard.vue
+++ webcurator-ui/src/views/Dashboard.vue
@@ -1,11 +1,27 @@
 <template>
   <div class="dashboard">
     <PageHeader title="Dashboard" />
+    <Button label="Fetch Targets" @click="showTargets" />
   </div>
 </template>
 
 <script setup lang="ts">
+
 import PageHeader from '@/components/PageHeader.vue';
+
+import {type UseFetchApis, useFetch} from '../rest.api';
+
+const rest: UseFetchApis=useFetch();
+
+const showTargets = () => {
+  const rsp= rest.get("./api/v1/targets");
+  rsp.then((data:any)=>{
+    console.log('fetch: ');
+    console.log(data);
+  }).catch((err:any)=>{
+      console.log(err.message);
+  });
+}
 </script>
 
 <style>
