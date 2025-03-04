312174f770078eafb5a4b9519d8456d2531c3a46
leefrank9527 <leefrank9527@gmail.com>
2025-03-05 10:26:59 +1300

    Forced to delete the folder of the visualization index when deleting normally

--- webcurator-core/src/main/java/org/webcurator/core/util/WctUtils.java
+++ webcurator-core/src/main/java/org/webcurator/core/util/WctUtils.java
@@ -15,6 +15,8 @@
  */
 package org.webcurator.core.util;
 
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.lang.SystemUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -22,9 +24,9 @@ import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.nio.charset.Charset;
 import java.nio.file.Files;
-import java.util.Calendar;
-import java.util.Date;
+import java.util.*;
 
 /**
  * A set of static utilities that can be reused throughout the WCT.
@@ -46,7 +48,7 @@ public class WctUtils {
      * @return True if the string is null or whitespace; otherwise false.
      */
     private static boolean isNullOrWhitespace(String str) {
-        return str == null || "".equals(str.trim());
+        return str == null || str.trim().isEmpty();
     }
 
     /**
@@ -237,6 +239,62 @@ public class WctUtils {
             }
         }
         return count;
+    }
+
+    public static String getFullPathOfCommand(String commandName) {
+        final List<String> ret = new ArrayList<>();
+        String command = "which " + commandName;
+        String[] commandList = command.split(" ");
+        try {
+            Process process = Runtime.getRuntime().exec(commandList);
+            int processStatus = process.waitFor();
+
+            if (processStatus != 0) {
+                log.error("Unable to process the command in a new thread, processStatus={}.", processStatus);
+            }
+            List<String> results = IOUtils.readLines(process.getInputStream(), Charset.defaultCharset());
+            ret.addAll(results);
+        } catch (Exception e) {
+            log.error("Unable to process the command in a new thread.", e);
+        }
+
+        if (!ret.isEmpty() && ret.get(0).endsWith(commandName)) {
+            return ret.get(0);
+        }
+        return null;
+    }
+
+    public static boolean processCommand(File targetDirectory, String[] commands) {
+        List<String> commandList = Arrays.asList(commands);
+        ProcessBuilder processBuilder = new ProcessBuilder(commandList);
+        processBuilder.directory(targetDirectory);
+        try {
+            Process process = processBuilder.inheritIO().start();
+            int processStatus = process.waitFor();
+
+            if (processStatus != 0) {
+                log.error("Process ended with a failed status: {}, commands: {}", processStatus, String.join(",", commands));
+            }
+            return processStatus == 0;
+        } catch (Exception e) {
+            log.error("Unable to process the command in a new thread.", e);
+            return false;
+        }
+    }
+
+    public static boolean forceDeleteDirectory(File toBeDeletedDirectory) {
+        if (toBeDeletedDirectory == null || !toBeDeletedDirectory.exists()) {
+            return true;
+        }
+        String os = SystemUtils.OS_NAME.toLowerCase();
+        if (SystemUtils.IS_OS_UNIX || os.contains("bsd")) {
+            String cmdRm = getFullPathOfCommand("rm");
+            String[] commands = {cmdRm, "-rf", toBeDeletedDirectory.getAbsolutePath()};
+            return processCommand(new File("/tmp"), commands);
+        } else {
+            //TODO: not supported
+            return false;
+        }
     }
 
     public static void main(String... args) {
