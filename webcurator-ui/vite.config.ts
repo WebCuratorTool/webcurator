b237f4eda51bb30a89716a01b902e19acea84958
leefrank9527 <leefrank9527@gmail.com>
2025-05-07 12:41:47 +1200

    upgraded the styles of ui

--- webcurator-ui/vite.config.ts
+++ webcurator-ui/vite.config.ts
@@ -1,25 +1,20 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
+import { PrimeVueResolver } from '@primevue/auto-import-resolver'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
+import Components from 'unplugin-vue-components/vite'
+import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: './',
-  plugins: [
-    vue(),
-    vueJsx(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  },
+  plugins: [vue(), vueJsx(), Components({ resolvers: [PrimeVueResolver()] })],
+  resolve: { alias: { '@': fileURLToPath(new URL('./src', import.meta.url)) } },
   server: {
     open: false,
-    port: 5173,
+    port: 5173
     // https: false,
     // hotOnly: false,
-  },
+  }
 })
