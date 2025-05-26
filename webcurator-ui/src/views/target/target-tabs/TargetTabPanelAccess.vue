f2e12761f68054cac3f7a299dcdc8ba63c071461
leefrank9527 <leefrank9527@gmail.com>
2025-05-26 19:57:47 +1200

    refactored the style of the access tabview

--- webcurator-ui/src/views/target/target-tabs/TargetTabPanelAccess.vue
+++ webcurator-ui/src/views/target/target-tabs/TargetTabPanelAccess.vue
@@ -1,59 +1,45 @@
 <script setup lang="ts">
-import { ref } from 'vue';
 import { useTargetAccessDTO } from '@/stores/target';
+import { ref } from 'vue';
 
-import WctFormField from '@/components/WctFormField.vue'
-import WctTabViewPanel from '@/components/WctTabViewPanel.vue'
-
+import WctFormField from '@/components/WctFormField.vue';
+import WctTabViewPanel from '@/components/WctTabViewPanel.vue';
 
 const data = useTargetAccessDTO();
 const targetAccess = data.targetAccess;
 
 defineProps<{
-    editing: boolean
-}>()
+  editing: boolean;
+}>();
 
-const accessZones = ref([
-    "Public",
-    "Onsite",
-    "Restricted",
-])
-
+const accessZones = ref(['Public', 'Onsite', 'Restricted']);
 </script>
 
 <template>
-    <WctTabViewPanel columns >
-        <div class="col">
-            <WctFormField label="Display Target">
-                <Checkbox v-if="editing" v-model="targetAccess.displayTarget" :binary="true"
-                    :disabled="!editing" />
-                <p v-else class="font-semibold">{{ targetAccess.displayTarget ? 'Yes' : 'No' }}</p>
-            </WctFormField>
-            <WctFormField label="Access Zone">
-                <Dropdown v-if="editing"
-                    v-model="targetAccess.accessZoneText"
-                    :options="accessZones"
-                />
-                <p v-else class="font-semibold">{{ targetAccess.accessZoneText }}</p>
-            </WctFormField>
-            <WctFormField label="Target Introductory Display Note">
-                <Textarea v-if="editing" 
-                    v-model="targetAccess.displayNote"
-                    :disabled="!editing" />
-                <p v-else class="font-semibold">{{ targetAccess.displayNote }}</p>  
-            </WctFormField>
-        </div>
-        <div class="col">
-            <WctFormField label="Reason for Display Change">
-                <Textarea v-if="editing" 
-                    v-model="targetAccess.displayChangeReason"
-                    :disabled="!editing" />
-                <p v-else class="font-semibold">{{ targetAccess.displayChangeReason }}</p>  
-            </WctFormField>
-        </div>
-    </WctTabViewPanel>
+  <WctTabViewPanel>
+    <div class="flex items-start justify-between gap-4">
+      <div class="w-full">
+        <WctFormField label="Display Target" inputId="display-target">
+          <Checkbox v-if="editing" v-model="targetAccess.displayTarget" :binary="true" :disabled="!editing" inputId="display-target" />
+          <p v-else class="font-semibold">{{ targetAccess.displayTarget ? 'Yes' : 'No' }}</p>
+        </WctFormField>
+        <WctFormField label="Access Zone">
+          <Dropdown v-if="editing" v-model="targetAccess.accessZoneText" :options="accessZones" />
+          <p v-else class="font-semibold">{{ targetAccess.accessZoneText }}</p>
+        </WctFormField>
+        <WctFormField label="Target Introductory Display Note">
+          <Textarea v-if="editing" v-model="targetAccess.displayNote" :disabled="!editing" />
+          <p v-else class="font-semibold">{{ targetAccess.displayNote }}</p>
+        </WctFormField>
+      </div>
+      <div class="w-full">
+        <WctFormField label="Reason for Display Change">
+          <Textarea v-if="editing" v-model="targetAccess.displayChangeReason" :disabled="!editing" />
+          <p v-else class="font-semibold">{{ targetAccess.displayChangeReason }}</p>
+        </WctFormField>
+      </div>
+    </div>
+  </WctTabViewPanel>
 </template>
 
-<style>
-
-</style>
+<style></style>
