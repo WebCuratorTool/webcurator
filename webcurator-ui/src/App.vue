50bba2179e1a164e65539c667f16d223facb0de6
jeremy-liss <jeremy.liss@dia.govt.nz>
2025-06-03 16:10:23 +1200

    Change theme to indigo

--- webcurator-ui/src/App.vue
+++ webcurator-ui/src/App.vue
@@ -8,7 +8,7 @@ import { togglePreset, toggleThemeMode } from './utils/themes';
 
 const loginStore = useLoginStore();
 toggleThemeMode('light');
-togglePreset('violet');
+togglePreset('indigo');
 </script>
 
 <template>
